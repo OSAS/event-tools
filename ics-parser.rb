4c8ea9869741ada0b736e9f5a1a032c8dbca724e
Garrett LeSage <garrett@lesage.us>
2015-01-19 12:10:43 +0100

    enable all events; add blanks for unknown info

--- ics-parser.rb
+++ ics-parser.rb
@@ -3,35 +3,21 @@
 require 'icalendar'
 require 'open-uri'
 require 'yaml'
+require 'word_wrap'
 
 ics = open('infra-at-scale-schedule.ics').read
 
 cal = Icalendar.parse(ics).first
 
-#puts ics.to_yaml
-#puts cal.x_wr_calname
-#puts cal.instance_variables
-#p cal.public_methods
-#puts cal.timezone.to_s
-
 events = []
 
-cal.events.take(1).each do |event|
-  #p event.instance_variables
-  #p event.public_methods
-  #puts event.to_yaml
-  #puts event.summary
-  #puts event.description
-  #puts event.dtstart
-  #puts event.dtend
-  #puts event.url
-
+cal.events.each do |event|
   event_item = {
     "title" => event.summary.strip,
-    "speaker" => "",
-    "start" => event.dtstart.strftime('%F %R'),
-    "end" => event.dtend.strftime('%F %R'),
-    "description" => event.description.strip
+    "speaker" => nil,
+    "start" => event.dtstart.strftime('%F %R %Z'),
+    "end" => event.dtend.strftime('%F %R %Z'),
+    "description" => event.description.strip.wrap(72)
   }
   events.push event_item
 end
@@ -40,8 +26,9 @@ conf_title = cal.x_wr_calname.first
 
 conference = {
   "title" => conf_title.strip,
-  "description" => "",
+  "description" => nil,
+  "location" => nil,
   "events" => events
 }
 
-puts conference.to_yaml
+puts conference.to_yaml(separator: nil, block: true, fold: true).gsub(/^(\s*- [\s\w]+:)/, "\n\\1").gsub(/^---$/, '').gsub(/: \|-/, ': |').strip
